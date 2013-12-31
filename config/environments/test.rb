86a9bb7402156c625a6542b907461d9a116cccf0
nxttmac <a.ertell@gmail.com>
2013-12-30 19:36:45 -0600

    Finish user signup + add SSL layer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,5 @@ Bustavote::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  ActiveModel::SecurePassword.min_cost = true
 end
