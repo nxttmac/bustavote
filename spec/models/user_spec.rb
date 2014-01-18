ebf8982e9089678e90842d4f080b0497f526416a
nxttmac <a.ertell@gmail.com>
2014-01-18 12:09:31 -0800

    Add user questions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:questions) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
